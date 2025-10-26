ef5314dea734161b568a6c92c677e738dca1f3ab
Hannah Connolly <connollyhannah93@gmail.com>
2025-10-25 23:29:39 -0400

    fix good pairs

--- warmup/good_pairs.py
+++ warmup/good_pairs.py
@@ -1,21 +1,14 @@
 class Solution:
-  def numGoodPairs(self, nums):
-    pairCount = 0
-    # TODO: Write your code here
+    def numGoodPairs(self, nums):
+        pairs = 0
 
-    # Map of values,
-    # key - value
-    # value - number of times seen (pairs)
-    value_map = {}
-    pairs = 0
+        for num in nums:
 
-    for num in nums:
+            if num in value_map:
+                value_map[num] += 1
+                pairs += value_map[num] - 1
 
-      if num in value_map:
-        value_map[num] += 1
-        pairs += value_map[num] - 1
+            else:
+                value_map[num] = 1
 
-      else:
-        value_map[num] = 1
-
-    return pairs
+        return pairs
